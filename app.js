bb6f55f6988b04485ed5a7a78f9dd2f57b099099
Ioannis Pialopoulos <ipialopoulos@gmail.com>
2026-04-16 04:21:23 +0300

    Added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express=require("express");
 const ollama = require('ollama').default;
 const Joi= require('joi');
+const cors = require('cors');
 const app=express();
 
 app.use(express.json());
+app.use(cors()); 
 
 stories = [];
 
